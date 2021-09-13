8ab9001b1231f852817b78137a18c5a8cbb01383
Nguyen Thanh Tung <nttung997@gmail.com>
2021-09-13 22:12:48 +0700

    fix UI

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function decode() {
-    let code = document.getElementById('floatingTextarea').value
-    if (null != code && "" != code) {
-        var t = -1 == code.indexOf(" "); t || (code = code.split(" "));
-        for (var result = "", a = 0; a < code.length; a += t ? 2 : 1) {
-            result += String.fromCharCode(parseInt(t ? code.substr(a, 2) : code[a], 16))
-        };
-    }
-    let link = document.getElementById('link')
-    link.href = result
-    link.innerText = result
+  let code = document.getElementById('floatingTextarea').value
+  if (null != code && "" != code) {
+    var t = -1 == code.indexOf(" "); t || (code = code.split(" "));
+    for (var result = "", a = 0; a < code.length; a += t ? 2 : 1) {
+      result += String.fromCharCode(parseInt(t ? code.substr(a, 2) : code[a], 16))
+    };
+  }
+  let link = document.getElementById('link')
+  link.href = result
+  link.innerText = result
 }
 
 // select everything
@@ -23,7 +23,7 @@ const todoItemsList = document.querySelector('.todo-items');
 let todos = [];
 
 // add an eventListener on form, and listen for submit event
-todoForm.addEventListener('submit', function(event) {
+todoForm.addEventListener('submit', function (event) {
   // prevent the page from reloading when submitting the form
   event.preventDefault();
   addTodo(todoInput.value); // call addTodo function with input box current value
@@ -55,15 +55,15 @@ function renderTodos(todos) {
   todoItemsList.innerHTML = '';
 
   // run through each item inside todos
-  todos.forEach(function(item) {
+  todos.forEach(function (item) {
     // check if the item is completed
-    const checked = item.completed ? 'checked': null;
+    const checked = item.completed ? 'checked' : null;
 
     // make a <li> element and fill it
     // <li> </li>
     const li = document.createElement('li');
     // <li class="item"> </li>
-    li.setAttribute('class', 'item');
+    li.setAttribute('class', 'item list-group-item flex-center');
     // <li class="item" data-key="20200708"> </li>
     li.setAttribute('data-key', item.id);
     /* <li class="item" data-key="20200708"> 
@@ -77,10 +77,10 @@ function renderTodos(todos) {
     }
 
     li.innerHTML = `
-      <input type="checkbox" class="checkbox" ${checked}>
-      ${item.name}
-      <button class="delete-button">X</button>
-    `;
+    <input type="checkbox" class="checkbox form-check-input" ${checked}>
+    ${item.name}
+    <button class="delete-button btn btn-sm btn-danger">X</button>
+  `;
     // finally add the <li> to the <ul>
     todoItemsList.append(li);
   });
@@ -108,7 +108,7 @@ function getFromLocalStorage() {
 
 // toggle the value to completed and not completed
 function toggle(id) {
-  todos.forEach(function(item) {
+  todos.forEach(function (item) {
     // use == not ===, because here types are different. One is number and other is string
     if (item.id == id) {
       // toggle the value
@@ -122,7 +122,7 @@ function toggle(id) {
 // deletes a todo from todos array, then updates localstorage and renders updated list to screen
 function deleteTodo(id) {
   // filters out the <li> with the id and updates the todos array
-  todos = todos.filter(function(item) {
+  todos = todos.filter(function (item) {
     // use != not !==, because here types are different. One is number and other is string
     return item.id != id;
   });
@@ -135,7 +135,7 @@ function deleteTodo(id) {
 getFromLocalStorage();
 
 // after that addEventListener <ul> with class=todoItems. Because we need to listen for click event in all delete-button and checkbox
-todoItemsList.addEventListener('click', function(event) {
+todoItemsList.addEventListener('click', function (event) {
   // check if the event is on checkbox
   if (event.target.type === 'checkbox') {
     // toggle the state
